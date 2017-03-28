baa1f775b3de7cd692bc466ec2d35e8fb4c413c0
kumar <letters2kumar@gmail.com>
2017-03-28 12:07:15 +0200

    Added form to the App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  loginForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      'email': '',
+      'password': ''
+    })
+  }
+
+  submitForm(value: any):void {
+    console.log('Form Submitted with values: ', value);
+  }
 }
