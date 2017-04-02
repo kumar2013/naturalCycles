6320ba0ea7972d6dde5427fbf3204b3a7451f4d7
kumar <letters2kumar@gmail.com>
2017-04-02 15:06:21 +0200

    Added validation to the form inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+import { EmailValidator } from './emailValidator.service';
+
+declare var Mailcheck: any;
 
 @Component({
   selector: 'app-root',
@@ -8,16 +13,41 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 
 export class AppComponent {
+  show: boolean = false;
+  emailSuggestion: string = '';
+
   loginForm: FormGroup;
 
   constructor(fb: FormBuilder) {
     this.loginForm = fb.group({
-      'email': '',
-      'password': ''
+      'email': [null, Validators.compose([Validators.required, EmailValidator.validate])],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12)])]
+    })
+
+    this.loginForm.valueChanges.subscribe(data => {
+      let emailAddress = data.email;
+
+      if (emailAddress.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+
+        Mailcheck.run({
+          email: emailAddress,
+          suggested: (suggestion) => {
+            this.show = true;
+            this.emailSuggestion = suggestion.full;
+          },
+          empty: () => {
+            this.show = false;
+          }
+        });
+      } else {
+        this.show = false;
+      }
     })
   }
 
   submitForm(value: any):void {
     console.log('Form Submitted with values: ', value);
+    this.loginForm.reset();
   }
 }
+
---
+++ src/app/emailValidator.service.ts
@@ -0,0 +1,10 @@
+export class EmailValidator {
+    static validate(control) {
+ 
+        if (!control.value || control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+            return null;
+        } else {
+            return { 'invalidEmailAddress': true };
+        }
+    }
+}
